41c75874f75a17c6f5215ce33ca1a69f7d9a1cf6
Rueben Anderson <rueand713@fullsail.edu>
2013-12-12 22:57:53 -0600

    f

--- fusion-android/gen/com/randerson/fusion/R.java
+++ fusion-android/gen/com/randerson/fusion/R.java
@@ -13,10 +13,58 @@ public final class R {
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
+    public static final class id {
+        public static final int backBtn=0x7f070001;
+        public static final int cancelButton=0x7f070008;
+        public static final int country=0x7f070005;
+        public static final int doneButton=0x7f070007;
+        public static final int email=0x7f070006;
+        public static final int password=0x7f070004;
+        public static final int refreshBtn=0x7f070002;
+        public static final int scoreTable=0x7f070000;
+        public static final int username=0x7f070003;
+    }
     public static final class layout {
-        public static final int main=0x7f030000;
+        public static final int leaderboard=0x7f030000;
+        public static final int main=0x7f030001;
+    }
+    public static final class raw {
+        public static final int countries=0x7f040000;
     }
     public static final class string {
-        public static final int app_name=0x7f040000;
+        public static final int app_key=0x7f050001;
+        public static final int app_name=0x7f050000;
+        public static final int app_secret=0x7f050002;
+        public static final int backButton=0x7f05000b;
+        public static final int cancelButton=0x7f050008;
+        public static final int country=0x7f050005;
+        public static final int directions=0x7f05000d;
+        public static final int email=0x7f050006;
+        public static final int leaderboardTitle=0x7f05000a;
+        public static final int okButton=0x7f050007;
+        public static final int password=0x7f050004;
+        public static final int refreshButton=0x7f05000c;
+        public static final int username=0x7f050003;
+        public static final int welcome=0x7f050009;
+    }
+    public static final class style {
+        /** 
+        Base application theme for API 14+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 14+ devices.
+    
+ API 14 theme customizations can go here. 
+
+        Base application theme for API 14+. This theme completely replaces
+        AppBaseTheme from BOTH res/values/styles.xml and
+        res/values-v11/styles.xml on API 14+ devices.
+    
+ API 14 theme customizations can go here. 
+         */
+        public static final int AppBaseTheme=0x7f060000;
+        public static final int HeaderStyle=0x7f060003;
+        public static final int SubTextStyle=0x7f060004;
+        public static final int TextStyle=0x7f060001;
+        public static final int TitleStyle=0x7f060002;
     }
 }
