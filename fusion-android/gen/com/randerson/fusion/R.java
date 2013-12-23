4a96116f706c52af10b669e3fddf5ae7ce2b3a4d
Rueben Anderson <rueand713@fullsail.edu>
2013-12-22 22:48:45 -0600

    Finalized

--- fusion-android/gen/com/randerson/fusion/R.java
+++ fusion-android/gen/com/randerson/fusion/R.java
@@ -33,26 +33,30 @@ public final class R {
         public static final int countries=0x7f040000;
     }
     public static final class string {
+        public static final int access_token=0x7f050005;
+        public static final int access_token_secret=0x7f050006;
         public static final int app_key=0x7f050001;
         public static final int app_name=0x7f050000;
         public static final int app_secret=0x7f050002;
-        public static final int backButton=0x7f05000b;
-        public static final int cancelButton=0x7f050008;
-        public static final int country=0x7f050005;
-        public static final int directions=0x7f050013;
-        public static final int email=0x7f050006;
-        public static final int leaderboardTitle=0x7f05000a;
-        public static final int okButton=0x7f050007;
-        public static final int origin=0x7f050010;
-        public static final int password=0x7f050004;
-        public static final int query=0x7f050011;
-        public static final int queryDirections=0x7f050012;
-        public static final int refreshButton=0x7f05000c;
-        public static final int score=0x7f05000f;
-        public static final int user=0x7f05000d;
-        public static final int userLevel=0x7f05000e;
-        public static final int username=0x7f050003;
-        public static final int welcome=0x7f050009;
+        public static final int backButton=0x7f05000f;
+        public static final int cancelButton=0x7f05000c;
+        public static final int country=0x7f050009;
+        public static final int directions=0x7f050017;
+        public static final int email=0x7f05000a;
+        public static final int leaderboardTitle=0x7f05000e;
+        public static final int okButton=0x7f05000b;
+        public static final int origin=0x7f050014;
+        public static final int password=0x7f050008;
+        public static final int query=0x7f050015;
+        public static final int queryDirections=0x7f050016;
+        public static final int refreshButton=0x7f050010;
+        public static final int score=0x7f050013;
+        public static final int twiter_key=0x7f050004;
+        public static final int twitter_secret=0x7f050003;
+        public static final int user=0x7f050011;
+        public static final int userLevel=0x7f050012;
+        public static final int username=0x7f050007;
+        public static final int welcome=0x7f05000d;
     }
     public static final class style {
         /** 
